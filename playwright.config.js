02df6d92285fa05f1b6a5662739835a94c030413
amalxaviour16 <amalxaviour16@gmail.com>
2025-08-15 15:29:55 +0530

    added-scripts

--- playwright.config.js
+++ playwright.config.js
@@ -11,6 +11,7 @@ const config = {
   use: {
     browserName: "chromium",
     headless: false,
+    screenshot: "on",
   },
 };
 module.exports = config;
--- tests/specs/01-login-flow.spec.js
+++ tests/specs/01-login-flow.spec.js
@@ -1,16 +1,43 @@
 import { test, expect } from "@playwright/test";
 //const LoginPage = require("../page-objects/login-page.js");
+import AdminUser from "../page-objects/adminuser-page.js";
 import LoginPage from "../page-objects/login-page.js";
 import HomePage from "../page-objects/home-page.js";
 import loginData from "../test-data/login-data.json";
 
-test.describe("Full flow of adding admin user", () => {
-  test("Login as Admin", async ({ page }) => {
+let page;
+let context;
+
+test.describe.serial("End to end script to create admin user", () => {
+  test("Login as Admin", async ({ browser }) => {
+    context = await browser.newContext();
+    page = await context.newPage();
     const loginPage = new LoginPage(page);
     const homePage = new HomePage(page);
     await loginPage.launchUrl();
     await loginPage.login(loginData.username, loginData.password);
     // await page.waitForTimeout(5000);
     await expect(homePage.$profileName).toBeVisible();
+  });
+
+  test("Navigate to admin user page", async () => {
+    const adminUserPage = new AdminUser(page);
+    await adminUserPage.clickAdminUser();
+  });
+
+  test("Click on New button", async () => {
+    const adminUserPage = new AdminUser(page);
+    await adminUserPage.clickNewUser();
+  });
+
+  test("create user", async () => {
+    const adminUserPage = new AdminUser(page);
+    await adminUserPage.fillDetails("palywright", "playwright", "Partner");
+  });
+
+  test("Click on Save button", async () => {
+    const adminUserPage = new AdminUser(page);
+    await adminUserPage.clickSaveButton();
+    await page.waitForTimeout(5000);
   });
 });
